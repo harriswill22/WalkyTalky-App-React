2de650370a227df86d156fabb2d943d17a18aa1d
Will Harris <Harriswill22@yahoo.com>
2018-12-06 12:38:16 -0500

    basic functionality with 3 components

---
+++ src/OutputBox.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const OutputBox = (props)  =>{
+    return(
+        <div className="output-box">
+        {props.theText}
+        </div>
+    );
+}
+
+export default OutputBox;
