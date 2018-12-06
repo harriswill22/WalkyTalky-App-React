79d4a62c660033b5c01b183c786ec1c2bf4cf1dd
Will Harris <Harriswill22@yahoo.com>
2018-12-06 13:47:20 -0500

    uses leetspeak for output

--- src/OutputBox.js
+++ src/OutputBox.js
@@ -1,9 +1,30 @@
 import React from 'react';
+const l33t ={
+    A: '4',
+    E: '3',
+    G: '6',
+    I: '1',
+    O: '0',
+    S: '5',
+    T: '7'
+}
+
+
+
+function toL33t(originalText) { 
+    const letterArray = originalText.split('');
+    const L33Array = letterArray.map(letter =>{
+        return l33t[letter.toUpperCase()] || letter;
+    });
+    return L33Array.join('');
+}
+
+
 
 const OutputBox = (props)  =>{
     return(
         <div className="output-box">
-        {props.theText}
+        {toL33t(props.theText)}
         </div>
     );
 }
